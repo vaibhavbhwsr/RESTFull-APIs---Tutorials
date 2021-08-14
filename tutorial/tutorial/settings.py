0aa6862935b45807c569211d3957924b3a009609
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-08-14 17:18:10 +0530

    Changed database for tutorial from postgres to sqlite and added .gitignore

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restdb',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
-        'HOST': '127.0.0.1',
-        'port': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
